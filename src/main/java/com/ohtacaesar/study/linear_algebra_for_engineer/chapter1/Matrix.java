656dd3378d98831dab720e7af010636de61dc8c7
ohtacaesar <ohtacaesar@gmail.com>
2019-11-21 23:42:36 +0900

    1.3.1 行列の定義と基本演算(途中)

--- src/main/java/com/ohtacaesar/study/linear_algebra_for_engineer/chapter1/Matrix.java
+++ src/main/java/com/ohtacaesar/study/linear_algebra_for_engineer/chapter1/Matrix.java
@@ -1,11 +1,19 @@
 package com.ohtacaesar.study.linear_algebra_for_engineer.chapter1;
 
 import com.ohtacaesar.study.linear_algebra_for_engineer.RealNumber;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Value;
 
+@Value
 public class Matrix {
 
   public static Matrix of(int m, int n, RealNumber[] realNumberArray) {
     return new Matrix(m, n, realNumberArray);
+  }
+
+  public static Matrix random(int m, int n) {
+    return new Matrix(m, n, RealNumber.randomArray(m * n));
   }
 
   // 行
@@ -13,6 +21,7 @@ public class Matrix {
   // 列
   public final int n;
 
+  @Getter(value = AccessLevel.NONE)
   private final RealNumber[] realNumberArray;
 
 
@@ -41,6 +50,47 @@ public class Matrix {
       newArray[i % n * m + i / n] = realNumberArray[i];
     }
     return Matrix.of(n, m, newArray);
+  }
+
+  public Matrix multiply(RealNumber scalar) {
+    RealNumber[] newArray = this.realNumberArray.clone();
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] = newArray[i].multiply(scalar);
+    }
+
+    return Matrix.of(this.m, this.n, newArray);
+  }
+
+  public Matrix add(Matrix matrix) {
+    assert this.m == matrix.m;
+    assert this.n == matrix.n;
+
+    RealNumber[] newArray = new RealNumber[this.realNumberArray.length];
+    for (int i = 0; i < this.realNumberArray.length; i++) {
+      newArray[i] = this.realNumberArray[i].add(matrix.realNumberArray[i]);
+    }
+
+    return Matrix.of(this.m, this.n, newArray);
+  }
+
+  public Matrix multiply(Matrix matrix) {
+    assert this.n == matrix.m;
+    int size = this.m * matrix.n;
+    RealNumber[] newArray = new RealNumber[size];
+
+    for (int i = 0; i < this.m; i++) {
+      for (int k = 0; k < matrix.n; k++) {
+        RealNumber r = RealNumber.of(0);
+        for (int j = 0; j < this.n; j++) {
+          r = r.add(
+              this.realNumberArray[i * this.n + j].multiply(
+                  matrix.realNumberArray[j * matrix.n + k]));
+        }
+        newArray[i * matrix.n + k] = r;
+      }
+    }
+
+    return Matrix.of(this.m, matrix.n, newArray);
   }
 
 }
--- src/test/java/com/ohtacaesar/study/linear_algebra_for_engineer/chapter1/MatrixTest.java
+++ src/test/java/com/ohtacaesar/study/linear_algebra_for_engineer/chapter1/MatrixTest.java
@@ -7,6 +7,17 @@ import org.junit.Test;
 public class MatrixTest {
 
   @Test
+  public void testEquals() {
+    Matrix a = Matrix.of(2, 3, RealNumber.arrayOf(1, 2, 3, 4, 5, 6));
+    Matrix b = Matrix.of(2, 3, RealNumber.arrayOf(1, 2, 3, 4, 5, 6));
+    Matrix c = Matrix.of(3, 2, RealNumber.arrayOf(1, 2, 3, 4, 5, 6));
+
+    Assert.assertEquals(a, a);
+    Assert.assertEquals(a, b);
+    Assert.assertNotEquals(a, c);
+  }
+
+  @Test
   public void testOf() {
     Matrix matrix = Matrix.of(3, 2, RealNumber.randomArray(6));
     Assert.assertNotNull(matrix);
@@ -40,5 +51,54 @@ public class MatrixTest {
     Assert.assertEquals(a.item(2, 1), b.item(1, 2));
     Assert.assertEquals(a.item(3, 1), b.item(1, 3));
     Assert.assertEquals(a.item(4, 1), b.item(1, 4));
+  }
+
+  @Test
+  public void testMultiplyWithScaler() {
+    RealNumber k = RealNumber.random();
+    Matrix a = Matrix.random(3, 2);
+    Matrix b = a.multiply(k);
+
+    for (int i = 1; i <= a.m; i++) {
+      for (int j = 1; j <= a.n; j++) {
+        Assert.assertEquals(b.item(i, j), a.item(i, j).multiply(k));
+      }
+    }
+  }
+
+  @Test
+  public void testAdd() {
+    Matrix a = Matrix.random(3, 2);
+    Matrix b = a.add(a);
+
+    for (int i = 1; i <= a.m; i++) {
+      for (int j = 1; j <= a.n; j++) {
+        Assert.assertEquals(b.item(i, j), a.item(i, j).multiply(RealNumber.of(2)));
+      }
+    }
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testAddWithDifferentSize() {
+    Matrix a = Matrix.random(3, 2);
+    Matrix b = Matrix.random(4, 2);
+
+    a.add(b);
+  }
+
+  @Test
+  public void testMultiplyWithMatrix() {
+    Matrix a = Matrix.of(2, 2, RealNumber.arrayOf(1, 1, 2, 2));
+    Matrix b = Matrix.of(2, 2, RealNumber.arrayOf(1, 2, 1, 2));
+    Matrix ab = Matrix.of(2, 2, RealNumber.arrayOf(2, 4, 4, 8));
+    Matrix ba = Matrix.of(2, 2, RealNumber.arrayOf(5, 5, 5, 5));
+    Assert.assertEquals(ab, a.multiply(b));
+    Assert.assertEquals(ba, b.multiply(a));
+
+    a = Matrix.of(2, 6, RealNumber.randomArray(12));
+    b = Matrix.of(6, 3, RealNumber.randomArray(18));
+    Matrix c = a.multiply(b);
+    Assert.assertEquals(a.m, c.m);
+    Assert.assertEquals(b.n, c.n);
   }
 }
